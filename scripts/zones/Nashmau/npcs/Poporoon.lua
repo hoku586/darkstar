5e380eb5ac6e8bfd792d84c66414a32dc0f1ecd9
hoku <jarrett586@hotmail.com>
2017-02-18 16:27:34 -0600

    Poporooon  Adding Corsiar AF to npc shop

--- scripts/zones/Nashmau/npcs/Poporoon.lua
+++ scripts/zones/Nashmau/npcs/Poporoon.lua
@@ -24,10 +24,22 @@ function onTrigger(player,npc)
     
 player:showText(npc,POPOROON_SHOP_DIALOG);
 
-stock = {0x3298,336,        -- Leather Highboots
-     0x3299,3438,        -- Lizard Ledelsens
-     0x329A,11172,        -- Studded Boots
-     0x329B,20532}        -- Cuir Highboots
+stock = {0x0880,48,        -- Fire Card
+     0x0881,48,        -- Ice Card
+     0x0882,48,        -- Wind Card
+     0x0883,48,        -- Earth Card
+     0x0884,48,        -- Thunder Card
+     0x0885,48,        -- Water Card
+     0x0886,48,        -- Light Card
+     0x0887,48,        -- Dark Card
+     0x16ee,10000,        -- Trump Card Case
+     0x1570,35200,        -- Samurai Die
+     0x1571,600,        -- Ninja Die
+     0x3BA2,50000,        -- Dragoon Die
+     0x3A51,50000,        -- Summoner Die
+     0x38Ba,50000,        -- Blue Mage Die
+     0x3Cf1,50000,        -- Corsar Die
+     0x3D45,50000}        -- Puppetmaster Die
  
 showShop(player, STATIC, stock);
 end; 
